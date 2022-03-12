7e2ab18c354012e1a727e81fd24afcf471ee3b2d
Hi-bro-nice-prog <96258670+Hi-bro-nice-prog@users.noreply.github.com>
2022-03-12 13:01:28 +0600

    Add files via upload

--- FinanceReportProcessorTest.java
+++ FinanceReportProcessorTest.java
@@ -10,13 +10,18 @@ public class FinanceReportProcessorTest {
         Payment y1 = new Payment("Ленин В.И.", 4, 3, 2022, 123);
         Payment z1 = new Payment("Путин В.В.", 5, 12, 2022, 1024);
         Payment w1 = new Payment("Ленин В.И.", 6, 3, 2022, 100);
-        Payment[] arr1 = new Payment[]{x1, y1, z1, w1};
-        FinanceReport obj1 = new FinanceReport(arr1, "Пушкин А.С.", 9, 3, 2022);
+        FinanceReport obj1 = new FinanceReport("Пушкин А.С.", 9, 3, 2022);
+        obj1.setPayment(x1);
+        obj1.setPayment(y1);
+        obj1.setPayment(z1);
+        obj1.setPayment(w1);
         Payment x2 = new Payment("Ленин В.И.", 3, 3, 2022, 100);
         Payment y2 = new Payment("Ленин В.И.", 4, 3, 2022, 123);
-        Payment w2 = new Payment("Ленин В.И.", 6, 3, 2022, 100);
-        Payment[] arr2 = new Payment[]{x2, y2, w2};
-        FinanceReport obj2 = new FinanceReport(arr2, "Пушкин А.С.", 9, 3, 2022);
+        Payment z2 = new Payment("Ленин В.И.", 6, 3, 2022, 100);
+        FinanceReport obj2 = new FinanceReport("Пушкин А.С.", 9, 3, 2022);
+        obj2.setPayment(x2);
+        obj2.setPayment(y2);
+        obj2.setPayment(z2);
         String str1 = obj2.toString();
         FinanceReportProcessor obj4 = new FinanceReportProcessor();
         FinanceReport obj3 = obj4.FullNameSymbol(obj1, 'Л');
@@ -28,6 +33,26 @@ public class FinanceReportProcessorTest {
 
     @Test
     public void sizeValue() {
-
+        Payment x1 = new Payment("Ленин В.И.", 3, 3, 2022, 100);
+        Payment y1 = new Payment("Ленин В.И.", 4, 3, 2022, 123);
+        Payment z1 = new Payment("Путин В.В.", 5, 12, 2022, 1024);
+        Payment w1 = new Payment("Ленин В.И.", 6, 3, 2022, 100);
+        FinanceReport obj1 = new FinanceReport("Пушкин А.С.", 9, 3, 2022);
+        obj1.setPayment(x1);
+        obj1.setPayment(y1);
+        obj1.setPayment(z1);
+        obj1.setPayment(w1);
+        Payment x2 = new Payment("Ленин В.И.", 3, 3, 2022, 100);
+        Payment z2 = new Payment("Ленин В.И.", 6, 3, 2022, 100);
+        FinanceReport obj2 = new FinanceReport("Пушкин А.С.", 9, 3, 2022);
+        obj2.setPayment(x2);
+        obj2.setPayment(z2);
+        String str1 = obj2.toString();
+        FinanceReportProcessor obj4 = new FinanceReportProcessor();
+        FinanceReport obj3 = obj4.sizeValue(obj1, 101);
+        String str2 = obj3.toString();
+        assertEquals(str1, str2);
+        System.out.println(str1);
+        System.out.println(str2);
     }
 }
--- FinanceReportTest.java
+++ FinanceReportTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class FinanceReportTest {
@@ -10,8 +12,11 @@ public class FinanceReportTest {
         Payment y = new Payment("Ленин В.И.", 4, 3, 2022, 123);
         Payment z = new Payment("Ленин В.И.", 5, 12, 2022, 1024);
         Payment w = new Payment("Ленин В.И.", 6, 3, 2022, 100);
-        Payment[] arr = new Payment[]{x, y, z, w};
-        FinanceReport exm = new FinanceReport(arr, "Пушкин А.С.", 9, 3, 2022);
+        FinanceReport exm = new FinanceReport("Пушкин А.С.", 9, 3, 2022);
+        exm.setPayment(x);
+        exm.setPayment(y);
+        exm.setPayment(z);
+        exm.setPayment(w);
         String str1 = "Автор: Пушкин А.С., дата: 09.03.2022 Платежи:[\n" +
                 "Плательщик: Ленин В.И., дата: 03.03.2022, сумма: 1 руб. 0 коп.,\n" +
                 "Плательщик: Ленин В.И., дата: 04.03.2022, сумма: 1 руб. 23 коп.,\n" +
@@ -19,6 +24,7 @@ public class FinanceReportTest {
                 "Плательщик: Ленин В.И., дата: 06.03.2022, сумма: 1 руб. 0 коп.]";
         String str2 = exm.toString();
         assertEquals(str1, str2);
+        System.out.println(str1);
         System.out.println(str2);
     }
 }
